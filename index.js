71799573109279b6e927b5fb38a330decb2e563d
SallyMulupi <mulupisally2gmail.com>
2022-06-18 23:21:36 +0300

    done

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Write your algorithm here
+    const leng = word.length;
+    //loop thru half of the string
+    for (let x = 0; x < leng / 2; x++) {
+        if (word[x] !== word[leng - 1 - x]) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+    word = prompt('Enter a word:')
+        //call the function
+    console.log(isPalindrome(robot));
+
+
 }
+
+
 
 /* 
   Add your pseudocode here
+  iterate from the start of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+if we reach the middle, and all the letters match, return true
 */
 
 /*
@@ -12,14 +33,14 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
